259795b2bb9bad8f8fb976050cc6fd58c14dc60f
tanrajp <https://tanrajp@bitbucket.org>
2014-04-08 10:48:40 +0100

    Initial commit

---
+++ WumpusGame/Cave.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusGame
+{
+    public class Cave
+    {
+        private static const int SIZE = 10;
+
+        private char[][] map;
+    }
+}
---
+++ WumpusGame/Player.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WumpusGame
+{
+    public class Player
+    {
+        public bool HasWeapon { get; set; }
+        public bool IsAlive { get; set; }
+        public int Gold { get; set; }
+        public Score score {get;set;}
+
+        public Player()
+        {
+            HasWeapon = false;
+            IsAlive = true;
+            Gold = 0;
+        }
+
+
+        public string Status()
+        {
+            return string.Format(@"[{0} points earned] {1}", -1, HasWeapon ? "You are armed and dangerous." : "You are weaponless.");
+        }
+    }
+}
